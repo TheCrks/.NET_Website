18f128e8ce466d2501e512b96e7e760e26ca1d9e
Umut Enes UZUN <85020724+TheCrks@users.noreply.github.com>
2023-08-23 12:49:03 +0300

    Add files via upload

---
+++ Model/AppModel.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.Model
+{
+    public class AppModel
+    {
+        public string appId { get; set; }
+
+        public string title { get; set; }
+        public string url { get; set; } 
+        public string imgUrl { get; set; }
+        public string released { get; set; }
+        public string reviewSummary { get; set; }
+        public string price { get; set; }
+
+    }
+}
---
+++ Model/CommentModel.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication1.Model
+{
+    public class CommentModel
+    {
+        public int Id { get; set; }
+        public int GameId { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public DateTime Time { get; set; }
+        public int Rating { get; set; }
+
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            CommentModel otherComment = (CommentModel)obj;
+            return this.Id == otherComment.Id;
+        }
+    }
+}
